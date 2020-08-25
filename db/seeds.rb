df4d6db36daca5e3871e812afec2997b82e88d3e
Dulan Perera <dulanp18@gmail.com>
2020-08-25 23:38:53 +1200

    yelp reviews with destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "cleaning database"
+Restaurant.destroy_all
+puts "Creating restaurants..."
+5.times do
+  restaurant = Restaurant.create(
+          name: Faker::Restaurant.name,
+          address: Faker::Address.street_address,
+          phone_number: Faker::PhoneNumber.cell_phone.to_s,
+          category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+          )
+  puts "Restaurant with id #{restaurant.id} has been created"
+end
+
+puts "Database has been created"
